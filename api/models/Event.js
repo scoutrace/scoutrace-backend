23816dd725daf4037c4f6005a4f4efb1bf89d53d
Soren Malling <soren.malling@gmail.com>
2014-04-06 15:00:31 +0200

    Add simple event model

---
+++ api/models/Event.js
@@ -0,0 +1,27 @@
+/**
+ * Event
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	attributes: {
+		name : {
+			type : 'string',
+			required : true
+		},
+		publicAvailable : {
+			type : 'boolean'
+		},
+		startDate : {
+			type : 'date'
+		},
+		endDate : {
+			type : 'date'
+		}
+	}
+
+};
