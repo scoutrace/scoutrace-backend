5d1ca84e95d4f5b0b5f0c736030a4e1deb3e202c
David Fotel <davsebamse@gmail.com>
2014-04-17 15:42:30 +0200

    Facebook is working nicely

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,4 @@
-
-
-
+var ids = require('./local.js').ids;
 var passport = require('passport')
 var FacebookStrategy = require('passport-facebook').Strategy;
 var TwitterStrategy = require('passport-twitter').Strategy;
@@ -8,33 +6,31 @@ var GithubStrategy = require('passport-github').Strategy;
 var GoogleStrategy = require('passport-google').Strategy;
 
 passport.serializeUser(function(user, done) {
-done(null, user);
+	done(null, user);
 });
 
 passport.deserializeUser(function(id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
+	User.findById(id, function (err, user) {
+    	done(err, user);
+	});
 });
 
 passport.use(new FacebookStrategy({
- clientID: ids.facebook.clientID,
- clientSecret: ids.facebook.clientSecret,
- callbackURL: ids.facebook.callbackURL
+	clientID: ids.facebook.clientID,
+	clientSecret: ids.facebook.clientSecret,
+	callbackURL: ids.facebook.callbackURL
 },
 function(accessToken, refreshToken, profile, done) {
- process.nextTick(function () {
-   return done(null, profile);
- });
-}
-));
+	process.nextTick(function () {
+		return done(null, profile);
+	});
+}));
 
 module.exports = {
- express: {
-    customMiddleware: function(app){
-      console.log('express midleware for passport');
-      app.use(passport.initialize());
-      app.use(passport.session());
-    }
-  }
+	express: {
+		customMiddleware: function(app){
+			app.use(passport.initialize());
+			app.use(passport.session());
+		}
+	}
 };
